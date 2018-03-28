01a7ad9d0ac571e9dd889862518a65ad807914dc
Senhung Wong <alexwongsenhung@gmail.com>
2018-03-28 00:50:14 -0700

    CMS-6: Landing Page Structure

---
+++ src/components/LandingPage/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Logo />
+        <Button />
+    </div>
+</template>
+
+<script>
+import Logo from '../Share/Logo';
+import Button from '../Share/Button';
+
+export default {
+    components: {
+        Logo,
+        Button
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Share/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Logo</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
