b4d7e9b719adb61883efbb55640384a71af3e3b3
Senhung Wong <alexwongsenhung@gmail.com>
2018-03-28 20:06:45 -0700

    CMS-7: Install Vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LandingPage from '../components/LandingPage/Layout.vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Router)
+Vue.use(Vuetify)
 
 export default new Router({
   routes: [
