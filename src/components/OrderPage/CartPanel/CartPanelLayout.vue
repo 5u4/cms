a0145584eeaecd8d5e2db6475056b5809bc71a4a
Senhung Wong <alexwongsenhung@gmail.com>
2018-03-30 11:58:22 -0700

    CMS-8: Cart Item Margin

--- src/components/OrderPage/CartPanel/CartPanelLayout.vue
+++ src/components/OrderPage/CartPanel/CartPanelLayout.vue
@@ -6,15 +6,15 @@
                 <h1>Cart</h1>
             </v-flex>
             <!-- Cart Items -->
-            <v-flex xs1>
+            <v-flex xs1 style="margin: 5px 0">
                 <CartItem></CartItem>
             </v-flex>
-            <v-flex xs1>
+            <v-flex xs1 style="margin: 5px 0">
                 <CartItem></CartItem>
             </v-flex>
             <!-- TotalPrice + Order -->
             <v-spacer></v-spacer>
-            <v-flex xs1>
+            <v-flex xs1 style="margin: 5px 0">
                 <v-card color="teal lighten-5">
                     <v-card-text>$20</v-card-text>
                     <v-btn flat block>ORDER</v-btn>
