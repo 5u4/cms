045702fb85c7cd628931e8b4db7cb1182adbec0d
Senhung Wong <alexwongsenhung@gmail.com>
2018-03-29 21:51:03 -0700

    CMS-8: Order Page Layout

---
+++ src/components/OrderPage/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-flex fill-height fluid>
+        <v-layout row fill-height>
+            <!-- Left Panel -->
+            <v-flex xs7 sm7 md8></v-flex>
+            <!-- Right Panel -->
+            <v-flex></v-flex>
+        </v-layout>
+    </v-flex>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LandingPage from '../components/LandingPage/Layout.vue';
+import OrderPage from '../components/OrderPage/Layout.vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css'
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'LandingPage',
       component: LandingPage
+    },
+    {
+      path: '/order',
+      name: 'OrderPage',
+      component: OrderPage
     }
   ]
 })
