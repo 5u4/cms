c67589fcd5ca6bab2a9af1f56690c01ee53bc9d5
Senhung Wong <alexwongsenhung@gmail.com>
2018-04-01 10:14:23 -0700

    CMS-9: Menu Refactor

--- src/components/OrderPage/ManuPanel/MenuItem/MenuItems.vue
+++ src/components/OrderPage/ManuPanel/MenuItem/MenuItems.vue
@@ -1,7 +1,7 @@
 <template>
     <v-layout row wrap align-content-start>
-        <v-flex v-for="item in getMenuItems" :key="item.id" xs3 style="margin: 7px 15px" @click="addMenuItemIntoCart(item.id)">
-            <MenuItem :name="item.name" :price="item.price.toFixed(2)" />
+        <v-flex v-for="item in getMenuItems" :key="item.id" xs3 style="margin: 7px 15px" >
+            <MenuItem :item="item" />
         </v-flex>
     </v-layout>
 </template>
@@ -17,32 +17,6 @@ export default {
     }),
     components: {
         MenuItem
-    },
-    methods: {        
-        addMenuItemIntoCart(id) {
-            /* Get Indices */
-            const menuIndex = this.$store.state.menuItems.map(item => item.id).findIndex(itemId => id == itemId);
-            const cartIndex = this.$store.state.cartItems.map(item => item.id).findIndex(itemId => id == itemId);
-
-            /* Exists in cart; update */
-            if (cartIndex >= 0) {
-                this.$store.commit('updateCartItemQuantity', {
-                    quantity: 1,
-                    item: this.getCartItems[cartIndex],
-                    itemPrice: this.getMenuItems[menuIndex].price
-                });
-            }
-
-            /* Not exists; push */
-            else {
-                this.$store.commit('pushMenuItemIntoCartItems', {
-                    id: id, 
-                    name: this.getMenuItems[menuIndex].name,
-                    price: this.getMenuItems[menuIndex].price,
-                    quantity: 1
-                });
-            }
-        }
     }
 }
 </script>
